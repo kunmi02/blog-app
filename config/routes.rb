944b29ffb0715f28d84da62ca234e3abfeebf5d6
kunmi02 <mrkhunmie@gmail.com>
2021-01-05 14:08:02 +0100

    Blog App

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  
+  resources :articles do
+    resources :comments
+  end
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+end
+# Rails.application.routes.draw do
+#   get "/articles", to: "articles#index"
+
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
